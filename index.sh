ce84cc5646a5f7dd94dc30e5430306a3e3616f8f
Vasilii Bodnariuk <i@vasilesk.ru>
2018-03-29 23:43:35 +0300

    time rebanance

--- index.sh
+++ index.sh
@@ -1,1 +1,6 @@
 ./docreader.py $@
+nohup ./make_dict.py > /dev/null 2> /dev/null < /dev/null &
+while [ ! -f trigger ]
+do
+  sleep 2
+done
--- make_dict.sh
+++ make_dict.sh
@@ -1,8 +1,2 @@
-#! /bin/sh
-nohup ./make_dict.py > /dev/null 2> /dev/null < /dev/null &
-while [ ! -f trigger ]
-do
-  sleep 2
-done
 sleep 1
 rm trigger
